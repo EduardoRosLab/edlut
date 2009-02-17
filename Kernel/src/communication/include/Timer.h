e567f91f4bcb546836170d0c4e4031dc115e055f
Jesus Garrido <jgarridoalcazar@users.noreply.github.com>
2009-02-17 20:00:10 +0000

    Licence Changes

--- Kernel/src/communication/include/Timer.h
+++ Kernel/src/communication/include/Timer.h
@@ -1,4 +1,18 @@
-//( timer.hpp
+/***************************************************************************
+ *                           EDLUTKernel.cpp  -  description               *
+ *                           -------------------                           *
+ * copyright            : (C) 2009 by Jesus Garrido and Richard Carrillo   *
+ * email                : jgarrido@atc.ugr.es                              *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef TIMER_H
 #define TIMER_H
--- Kernel/src/spike/include/WeightChange.h
+++ Kernel/src/spike/include/WeightChange.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+ *                           WeightChange.h                                *
+ *                           -------------------                           *
+ * copyright            : (C) 2009 by Jesus Garrido and Richard Carrillo   *
+ * email                : jgarrido@atc.ugr.es                              *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef WEIGHTCHANGE_H_
 #define WEIGHTCHANGE_H_
 
