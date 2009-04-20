bba65a9222ce6b660c07744ab1bf73179cc111a8
jgarridoalcazar <jgarridoalcazar@bbf8b4c0-fcf4-11dd-b507-8b4ecd5210be>
2009-04-20 12:22:45 +0000

    New Version 0.2 Same tables for different architectures (x86 and 64 bits)

---
+++ tags/EDLUTKernel0_2/include/simulation/Configuration.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+ *                           Configuration.h                               *
+ *                           -------------------                           *
+ * copyright            : (C) 2009 by Jesus Garrido and Richard Carrillo   *
+ * email                : jgarrido@atc.ugr.es                              *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CONFIGURATION_H_
+#define CONFIGURATION_H_
+
+/*!
+ * \file Configuration.h
+ *
+ * \author Jesus Garrido
+ * \author Richard Carrido
+ * \date August 2008
+ *
+ * This file defines some configuration constants for simulation.
+ */
+ 
+//#define min(a,b) (((a)<(b))?(a):(b))
+
+/*!
+ * Time of the next spike when no spike is predicted.
+ */
+#define NOPREDICTION -1.0
+
+/*!
+ * Multiplicative factor of synaptic weights.
+ */
+#define WEIGHTSCALE 1
+
+/*!
+ * Maximum number of information lines.
+ */
+#define MAXINFOLINES 5
+
+/*!
+ * Default refractary period
+ */
+#define DEF_REF_PERIOD 2e-3
+
+/*!
+ * Maximum number of state variables.
+ */
+#define MAXSTATEVARS 7
+
+/*!
+ * Maximum number of characters of a type identificator.
+ */
+#define MAXIDSIZE 32
+
+/*!
+ * Maximum number of characters of a type identificator.
+ */
+#define MAXIDSIZEC "32"
+
+#endif /*CONFIGURATION_H_*/
---
+++ tags/EDLUTKernel0_2/include/simulation/Event.h
@@ -0,0 +1,106 @@
+/***************************************************************************
+ *                           Event.h                                       *
+ *                           -------------------                           *
+ * copyright            : (C) 2009 by Jesus Garrido and Richard Carrillo   *
+ * email                : jgarrido@atc.ugr.es                              *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef EVENT_H_
+#define EVENT_H_
+
+/*!
+ * \file Event.h
+ *
+ * \author Jesus Garrido
+ * \date November 2008
+ *
+ * This file declares a class which abstracts a simulation event.
+ */
+ 
+#include <iostream>
+
+using namespace std;
+
+class Simulation;
+
+/*!
+ * \class Event
+ *
+ * \brief Simulation abstract event.
+ *
+ * This class abstract the concept of event.
+ *
+ * \author Jesus Garrido
+ * \date November 2008
+ */
+class Event{
+	
+	protected: 
+   		/*!
+   		 * Time when the event happens.
+   		 */
+   		double time;
+   
+   	public:
+   		
+   		/*!
+   		 * \brief Default constructor.
+   		 * 
+   		 * It creates and initializes a new event object.
+   		 */
+   		Event();
+   	
+   		/*!
+   		 * \brief Constructor with parameters.
+   		 * 
+   		 * It creates and initializes a new event with the parameters.
+   		 * 
+   		 * \param NewTime Time of the new event.
+   		 */
+   		Event(double NewTime);
+   		
+   		/*!
+   		 * \brief Class destructor.
+   		 * 
+   		 * It destroies an object of this class.
+   		 */
+   		virtual ~Event();
+   	
+   		/*!
+   		 * \brief It gets the event time.
+   		 * 
+   		 * It gets the event time.
+   		 * 
+   		 * \return The event time.
+   		 */
+   		double GetTime() const;
+   		
+   		/*!
+   		 * \brief It sets the event time.
+   		 * 
+   		 * It sets the event time.
+   		 * 
+   		 * \param NewTime The new event time.
+   		 */
+   		void SetTime (double NewTime);
+   	
+   		/*!
+   		 * \brief It process an event in the simulation.
+   		 * 
+   		 * It process the event in the simulation.
+   		 * 
+   		 * \param CurrentSimulation The simulation object where the event is working.
+   		 */
+   		virtual void ProcessEvent(Simulation * CurrentSimulation) = 0;
+};
+
+#endif /*SPIKE_H_*/
