55b2ba672895d4202e34d06e94dce8e05a896f8c
jgarridoalcazar <jgarridoalcazar@bbf8b4c0-fcf4-11dd-b507-8b4ecd5210be>
2010-11-25 09:45:23 +0000

    Making EDLUT compatible with Windows systems

---
+++ branches/Kernel/include/simulation/CommunicationEvent.h
@@ -0,0 +1,84 @@
+/***************************************************************************
+ *                           CommunicationEvent.h                          *
+ *                           -------------------                           *
+ * copyright            : (C) 2009 by Jesus Garrido and Richard Carrillo   *
+ * email                : jgarrido@atc.ugr.es                              *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef COMMUNICATIONEVENT_H_
+#define COMMUNICATIONEVENT_H_
+
+/*!
+ * \file CommunicationEvent.h
+ *
+ * \author Jesus Garrido
+ * \date November 2008
+ *
+ * This file declares a class which abstracts a simulation event for the communication time.
+ */
+ 
+#include <iostream>
+
+#include "./Event.h"
+
+using namespace std;
+
+class Simulation;
+
+/*!
+ * \class CommunicationEvent
+ *
+ * \brief Simulation abstract event for the communication time.
+ *
+ * This class abstract the concept of event for the communication time.
+ *
+ * \author Jesus Garrido
+ * \date November 2008
+ */
+class CommunicationEvent: public Event{
+	
+	public:
+   		
+   		/*!
+   		 * \brief Default constructor.
+   		 * 
+   		 * It creates and initializes a new event object.
+   		 */
+   		CommunicationEvent();
+   	
+   		/*!
+   		 * \brief Constructor with parameters.
+   		 * 
+   		 * It creates and initializes a new event with the parameters.
+   		 * 
+   		 * \param NewTime Time of the new event.
+   		 */
+   		CommunicationEvent(double NewTime);
+   		
+   		/*!
+   		 * \brief Class destructor.
+   		 * 
+   		 * It destroies an object of this class.
+   		 */
+   		~CommunicationEvent();
+   	
+   		/*!
+   		 * \brief It process an event in the simulation.
+   		 * 
+   		 * It process the event in the simulation.
+   		 * 
+   		 * \param CurrentSimulation The simulation object where the event is working.
+   		 */
+   		virtual void ProcessEvent(Simulation * CurrentSimulation);
+};
+
+#endif /*COMMUNICATIONEVENT_H_*/
---
+++ branches/Kernel/src/communication/ClientSocket.cpp
@@ -0,0 +1,23 @@
+/***************************************************************************
+ *                           ClientSocket.cpp                              *
+ *                           -------------------                           *
+ * copyright            : (C) 2009 by Jesus Garrido and Richard Carrillo   *
+ * email                : jgarrido@atc.ugr.es                              *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "../../include/communication/ClientSocket.h"
+
+ClientSocket::ClientSocket(string server_address, unsigned short tcp_port):CdSocket(CLIENT,server_address,tcp_port){
+}
+
+ClientSocket::~ClientSocket(){
+}
